41cbda0799b8e3b1b49c4abbf3a5a7d8b27f9e0d
PUGAZHENTHI S <132038855+pugazh001@users.noreply.github.com>
2024-05-22 08:26:03 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,6 +15,13 @@ app.use(cors(
         credentials:true
     }
 ));
+app.options('/api/user/signup', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://sales-management-frontend-six.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.send();
+});
+
 app.use(express.json());
 
 
